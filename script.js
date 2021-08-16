dda1086f4d68658b34f1ed9d27a33dffb0d985da
Anna Kotova <annaxkotova@gmail.com>
2021-08-16 22:47:23 +0200

    batail gagnée

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 let lsArticles = localStorage.getItem("melons"); //LOCALE STORAGE
 
-let ajout = document.querySelector("#ajout"); //bouton ajout au panier
 
-//affichage nombre d'articles
+
+// pour affichage correct de nombre d'articles
+let melonsPanier = document.querySelector("#numero_melons_panier");
+
+let ajout = document.querySelector("#ajout"); //bouton ajout au panier
 ajout.addEventListener('click', function (e){
     let numeroArticles = document.querySelector("#numero_articles").value; //NUMERO SAISIE
     if (numeroArticles){    //si on a le valeur saisi on peut appeler la fonction
-        sumArticles = parseInt(numeroArticles) + Number(lsArticles); //numeroArticle était string
+        sumArticles = parseInt(numeroArticles) + Number(lsArticles); //tout était string
         localStorage.setItem("melons", sumArticles);
-        console.log(lsArticles);
+        lsArticles = localStorage.getItem("melons");
+        melonsPanier.innerHTML = lsArticles;
     }  
 })
 
 
 // VIDER UN PANIER
 let clear = document.querySelector("#clear");
-
 clear.addEventListener('click', function (e){
-localStorage.removeItem("melons");
-lsArticles = 0;
+    localStorage.removeItem("melons");
+    lsArticles = localStorage.getItem("melons");
+    melonsPanier.innerHTML = "0";
 })
 
+if (lsArticles === null) {
+    melonsPanier.innerHTML = "0";
+}
 
-let melonsPanier = document.querySelector("#numero_melons_panier");
-let numeroArticlesText = document.createTextNode(lsArticles);
-    melonsPanier.appendChild(numeroArticlesText);
 
+//Porquoi ça marche avec innerHTML mais ne marche pas avec appenchilds et tout ça?
 
 
